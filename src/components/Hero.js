3aa717a3107e808ab7f4452ac6af5f401060828b
Jmro7 <juanmareyesortiz@gmail.com>
2023-11-27 19:45:11 -0600

    Modify npm package stuff

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,125 +1,150 @@
 import React from "react"
 import styled from "styled-components"
 import Links from "../constants/sociallinks"
-import Part from "react-particles-js"
+// import Particles from "react-tsparticles"
 import { Link } from "gatsby"
 
 const Hero = () => {
   return (
     <HeroHeader>
       {/* Particles Component with the right speficiations*/}
-      <Particles
-        params={{
-          particles: {
-            number: {
-              value: 90,
-              density: {
-                enable: true,
-                value_area: 315,
-              },
-            },
-            color: {
-              value: "#ff7043",
-            },
-            shape: {
-              type: "circle",
-              stroke: {
-                width: 0,
-                color: "#000000",
-              },
-              polygon: {
-                nb_sides: 5,
-              },
-              image: {
-                src: "img/github.svg",
-                width: 100,
-                height: 100,
-              },
-            },
-            opacity: {
-              value: 0.5,
-              random: true,
-              anim: {
-                enable: true,
-                speed: 0.5,
-                opacity_min: 0.2,
-                sync: false,
-              },
-            },
-            size: {
-              value: 3,
-              random: true,
-              anim: {
-                enable: true,
-                speed: 1,
-                size_min: 0.1,
-                sync: false,
-              },
-            },
-            line_linked: {
-              enable: false,
-              distance: 150,
-              color: "#ffffff",
-              opacity: 0.4,
-              width: 1,
-            },
-            move: {
+      {/* <Partt
+      options={{
+        fullScreen: {
+          enable: true,
+          zIndex: 0
+        },
+        particles: {
+          number: {
+            value: 200,
+            limit: 300,
+            density: {
               enable: true,
-              speed: 0.5,
-              direction: "none",
-              random: true,
-              straight: false,
-              out_mode: "out",
-              bounce: false,
-              attract: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 1200,
-              },
-            },
+              value_area: 800
+            }
           },
-          interactivity: {
-            detect_on: "canvas",
-            events: {
-              onhover: {
-                enable: false,
-                mode: "repulse",
-              },
-              onclick: {
-                enable: false,
-                mode: "push",
-              },
-              resize: true,
-            },
-            modes: {
-              grab: {
-                distance: 400,
-                line_linked: {
-                  opacity: 1,
-                },
-              },
-              bubble: {
-                distance: 400,
-                size: 40,
-                duration: 2,
-                opacity: 8,
-                speed: 3,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-              push: {
-                particles_nb: 4,
-              },
-              remove: {
-                particles_nb: 2,
-              },
-            },
+          color: {
+            value: "#ffffff"
           },
-          retina_detect: true,
-        }}
-      />
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#000000"
+            },
+            polygon: {
+              nb_sides: 5
+            },
+            image: {
+              src: "images/github.svg",
+              width: 100,
+              height: 100
+            }
+          },
+          opacity: {
+            value: 0.5,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0.5,
+              sync: false
+            }
+          },
+          size: {
+            value: 30,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 10,
+              size_min: 10,
+              sync: false
+            }
+          },
+          line_linked: {
+            enable: true,
+            distance: 100,
+            color: "#ffffff",
+            opacity: 1,
+            width: 1
+          },
+          move: {
+            enable: true,
+            speed: 3,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200
+            }
+          }
+        },
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onHover: {
+              enable: true,
+              mode: "bubble",
+              parallax: {
+                enable: false,
+                force: 60,
+                smooth: 10
+              }
+            },
+            onClick: {
+              enable: true,
+              mode: "push"
+            },
+            resize: true
+          },
+          modes: {
+            grab: {
+              distance: 400,
+              lineLinked: {
+                opacity: 1
+              }
+            },
+            bubble: {
+              distance: 400,
+              size: 100,
+              duration: 2,
+              opacity: 1,
+              speed: 2
+            },
+            repulse: {
+              distance: 200
+            },
+            push: {
+              particles_nb: 4
+            },
+            remove: {
+              particles_nb: 2
+            }
+          }
+        },
+        backgroundMask: {
+          enable: true,
+          cover: {
+            color: {
+              value: {
+                r: 0,
+                g: 0,
+                b: 0
+              }
+            }
+          }
+        },
+        retina_detect: true,
+        fps_limit: 60,
+        background: {
+          image: "url('https://particles.js.org/images/background3.jpg')"
+        }
+      }} */}
+      {/* /> */}
       <HeroCenter>
         <HeroInfo>
           <div>
@@ -157,21 +182,21 @@ const Underline = styled.div`
   height: 0.25rem;
   background: ${({ theme }) => theme.colorPrimary5};
 `
-//Particles component positioning
-const Particles = styled(Part)`
-  position: absolute;
-  margin-top: -5rem;
-  padding-top: 5rem;
-  align-items: center;
-  width: 100%;
-  margin: 0 auto;
-  height: 70vh;
+// Particles component positioning
+// const Partt = styled(Particles)`
+//   position: absolute;
+//   margin-top: -5rem;
+//   padding-top: 5rem;
+//   align-items: center;
+//   width: 100%;
+//   margin: 0 auto;
+//   height: 70vh;
 
-  @media screen and (max-width: 992px) {
-    height: 65vh;
-    opacity: 1;
-  }
-`
+//   @media screen and (max-width: 992px) {
+//     height: 65vh;
+//     opacity: 1;
+//   }
+// `
 const HeroHeader = styled.header`
   background: ${({ theme }) => theme.background};
   height: 80vh;
